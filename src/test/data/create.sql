69b8f394ad738c2da60fda39ac55194b07553872
Bastian Bowe <bastian.bowe@gmail.com>
2010-12-12 20:35:07 +0100

    Fixed bug 688290 "NULL values with converters error on fetch."

--- src/test/data/create.sql
+++ src/test/data/create.sql
@@ -2,6 +2,7 @@ create table konto (
 "KONTO_ID" TIMESTAMP default CURRENT_TIMESTAMP not null,
 "KONTO_NR" INTEGER not null,
 "SALDO" DECIMAL default 0.0 not null,
+"SPERRE" DECIMAL,
 primary key ("KONTO_ID")
 );
 
