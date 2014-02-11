01283b183460d9d7a9bf5e3840f7a65a79d423ad
baztian <bastian.dev@gmail.com>
2014-02-11 17:51:06 +0100

    Fix typos

--- ci/before_install_jython.sh
+++ ci/before_install_jython.sh
@@ -3,7 +3,7 @@ set -e
 
 pip install jip==0.7
 jip install org.python:$JYTHON
-_JYTHON_BASENAME=${JYTHON/:/.}
+_JYTHON_BASENAME=${JYTHON/:/-}
 java -jar $VIRTUAL_ENV/javalib/${_JYTHON_BASENAME}.jar -s -d $VIRTUAL_ENV/jython
 touch requirements.txt
 virtualenv -p $VIRTUAL_ENV/jython/bin/jython ~/myvirtualenv
