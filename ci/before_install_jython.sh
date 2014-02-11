1188f14bef41924afa0f573a87bb04d5a952e238
baztian <bastian.dev@gmail.com>
2014-02-11 18:30:40 +0100

    Fix typo. Setup compatible virtualenv for Jython < 2.6

--- ci/before_install_jython.sh
+++ ci/before_install_jython.sh
@@ -6,4 +6,11 @@ jip install org.python:$JYTHON
 _JYTHON_BASENAME=${JYTHON/:/-}
 java -jar $VIRTUAL_ENV/javalib/${_JYTHON_BASENAME}.jar -s -d $VIRTUAL_ENV/jython
 touch requirements.txt
+BEFORE_PY_26=$($VIRTUAL_ENV/jython/bin/jython -c "import sys; print sys.version_info < (2, 6)")
+if [ "$BEFORE_PY_26" == "True" ]
+then
+    # Travis CI virtualenv version is greater 1.9.1, which was the
+    # last version compatible with Python version before 2.6
+    pip install virtualenv==1.9.1
+fi
 virtualenv -p $VIRTUAL_ENV/jython/bin/jython ~/myvirtualenv
