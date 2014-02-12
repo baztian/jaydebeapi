eb57a044536b594efeac5cee26b80e92060cb273
baztian <bastian.dev@gmail.com>
2014-02-12 23:12:51 +0100

    Run database specific tests in separate Travis builds

--- test/testsuite.py
+++ test/testsuite.py
@@ -2,33 +2,26 @@
 """Run unittests in the `tests` directory."""
 
 from optparse import OptionParser
-import os
 import sys
 
-def adjust_sys_path(dir_name='libs'):
-    """
-    Patch to search the libs folder. At the moment, I believe it's unable to
-    find .egg's, but it does search libs for imports before anything else.
-    """
-    root_dir = os.path.dirname(os.path.abspath(__file__))
-    sys.path.insert(0, os.path.join(root_dir, dir_name))
-
-adjust_sys_path('libs')
-adjust_sys_path('src')
-
-import unittest2
+import unittest2 as unittest
 
 def main():
     parser = OptionParser()
     parser.add_option("-x", "--xml", action="store_true", dest="xml",
                   help="write test report in xunit file format")
     (options, args) = parser.parse_args(sys.argv)
-    suite = unittest2.loader.TestLoader().discover('src/test')
+    loader = unittest.defaultTestLoader
+    names = args[1:]
+    if names:
+        suite = loader.loadTestsFromNames(names)
+    else:
+        suite = loader.discover('test')
     if options.xml:
         import xmlrunner
         runner = xmlrunner.XMLTestRunner(output='build/test-reports')
     else:
-        runner = unittest2.TextTestRunner(verbosity=2)
+        runner = unittest.TextTestRunner(verbosity=2)
     result = runner.run(suite)
     if result.wasSuccessful():
         return 0
