749f0600f006e7ed4a89f948388c32038e446545
baztian <bastian.dev@gmail.com>
2014-02-12 10:51:26 +0100

    Make pip work for Jython < 2.6 by favouring distribute over setuptools. Disable ssl

--- ci/before_install_jython.sh
+++ ci/before_install_jython.sh
@@ -2,25 +2,25 @@
 set -e
 
 pip install jip==0.7
-jip install org.python:$JYTHON
-_JYTHON_BASENAME=${JYTHON/:/-}
+jip install $JYTHON
+NON_GROUP_ID=${JYTHON#*:}
+_JYTHON_BASENAME=${NON_GROUP_ID/:/-}
 OLD_VIRTUAL_ENV=$VIRTUAL_ENV
-deactivate
-java -jar $OLD_VIRTUAL_ENV/javalib/${_JYTHON_BASENAME}.jar -s -d ~/jython
-touch requirements.txt
-BEFORE_PY_26=$(~/jython/bin/jython -c "import sys; print sys.version_info < (2, 6)")
+java -jar $OLD_VIRTUAL_ENV/javalib/${_JYTHON_BASENAME}.jar -s -d $HOME/jython
+
+BEFORE_PY_26=$($HOME/jython/bin/jython -c "import sys; print sys.version_info < (2, 6)")
 if [ "$BEFORE_PY_26" == "True" ]
 then
     # Travis CI virtualenv version is greater 1.9.1, which was the
     # last version compatible with Python version before 2.6
-    #curl https://raw.github.com/pypa/pip/master/contrib/get-pip.py -o get-pip.py
-    #curl http://peak.telecommunity.com/dist/ez_setup.py -o ez_setup.py
-    #$VIRTUAL_ENV/jython/bin/jython get-pip.py
-    #$VIRTUAL_ENV/jython/bin/jython ez_setup.py
-    #$VIRTUAL_ENV/jython/bin/easy_install install virtualenv==1.9.1
-    #sudo pip install virtualenv==1.9.1
-    sudo pip install virtualenv==1.7.1.2
+    pip install virtualenv==1.9.1
+    # No SSL support for Jython
+    mkdir $HOME/.pip
+    cat > $HOME/.pip/pip.conf <<EOF
+[install]
+insecure = true
+EOF
 fi
-which virtualenv
 virtualenv --version
-virtualenv -p ~/jython/bin/jython ~/myvirtualenv
+# --distribute is a workaround as setuptools don't install on Jython properly
+virtualenv --distribute -p $HOME/jython/bin/jython $HOME/myvirtualenv
--- ci/before_install_nonjython.sh
+++ ci/before_install_nonjython.sh
@@ -2,4 +2,4 @@
 set -e
 
 cat requirements_python.txt >> requirements.txt
-ln -s $VIRTUAL_ENV ~/myvirtualenv
+ln -s $VIRTUAL_ENV $HOME/myvirtualenv
