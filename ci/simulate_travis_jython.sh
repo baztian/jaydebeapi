f17d2f51494742b556d4d9db8d0991cef8031962
baztian <bastian.dev@gmail.com>
2014-02-12 12:54:22 +0100

    Write simulation script for travis build to avoid unnecessary Travis build time usage

---
+++ ci/simulate_travis_jython.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+set -e
+
+export BACKEND=hsqldb
+export JYTHON=org.python:jython-installer:2.5.3
+#export JYTHON=org.python:jython-installer:2.7-b1
+_JIP_HOME=$HOME/.jip
+export HOME=`mktemp -d`
+export TRAVIS_BUILD_DIR=$HOME/baztian/jaydebeapi
+git clone "$(git rev-parse --show-toplevel)" $TRAVIS_BUILD_DIR
+cd $TRAVIS_BUILD_DIR
+
+mkdir -p $HOME/.jip/cache/org.python/
+cp -r $_JIP_HOME/cache/org.python/jython-installer* $HOME/.jip/cache/org.python/
+
+virtualenv $HOME/orig
+source $HOME/orig/bin/activate
+pip install --upgrade virtualenv
+
+virtualenv $HOME/new
+source $HOME/new/bin/activate
+
+# required as new virtualenv doesn't have pip installed virtualenv
+pip install --upgrade virtualenv
+
+$TRAVIS_BUILD_DIR/ci/before_install_jython.sh
+
+source $HOME/myvirtualenv/bin/activate
+
+[ -x requirements.txt ] && pip install -r requirements.txt
+pip install jip==0.7
+pip install -e .
+jip install org.xerial:sqlite-jdbc:3.7.2
+jip install org.hsqldb:hsqldb:1.8.0.10
+
+export CLASSPATH=$VIRTUAL_ENV/javalib/*
+python test.py
+
+echo Remove ${HOME}?
+read
+rm -r $HOME
