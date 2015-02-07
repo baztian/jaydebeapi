de360ebc051bb046fd7f17a8d0600f9514304e0b
baztian <bastian.dev@gmail.com>
2015-02-07 20:57:18 +0100

    Use travis' container-based infrastructure

--- ci/before_install.sh
+++ ci/before_install.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 set -e
 
-sudo apt-get update -qq
-sudo apt-get install -qq openjdk-7-jdk openjdk-7-jre
+# JDK is not required for JPype1 anymore
+# sudo apt-get update -qq
+# sudo apt-get install -qq openjdk-7-jdk openjdk-7-jre
 
 [ -n "$JYTHON" ] && "${TRAVIS_BUILD_DIR}/ci/before_install_jython.sh" || "${TRAVIS_BUILD_DIR}/ci/before_install_nonjython.sh"
 
