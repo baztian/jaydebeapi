ae4acb64eed9e5862b888b9b808cff62d7a40eea
tdawber <tdawber@atlassian.com>
2016-12-28 12:41:57 +1100

    add error tests

--- test/test_integration.py
+++ test/test_integration.py
@@ -212,14 +212,14 @@ class IntegrationTestBase(object):
         try:
             cursor.execute("dummy stmt")
         except jaydebeapi.DatabaseError as e:
-            self.assertEquals(str(e), "java.sql.SQLException: expected")
+            self.assertEquals(str(e).split(" ")[0], "java.sql.SQLException:")
 
     def test_sql_exception_on_prepared_execute(self):
         cursor = self.conn.cursor()
         try:
             cursor.execute("dummy stmt ?", (18,))
         except jaydebeapi.DatabaseError as e:
-            self.assertEquals(str(e), "java.sql.SQLException: expected")
+            self.assertEquals(str(e).split(" ")[0], "java.sql.SQLException:")
 
 class SqliteTestBase(IntegrationTestBase):
 
