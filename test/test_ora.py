094ae1d1c9adfe7b759e50f0c2833e2633734790
baztian <bastian.dev@gmail.com>
2017-03-21 19:58:11 +0100

    Fix test work with changed api.

--- test/test_ora.py
+++ test/test_ora.py
@@ -1,8 +1,8 @@
 import jaydebeapi
-driver, driver_args = 'oracle.jdbc.OracleDriver', \
-   ['jdbc:oracle:thin:@//localhost:49161/xe',
-    'user', 'password']
-conn = jaydebeapi.connect(driver, driver_args)
+driver, url, driver_args = 'oracle.jdbc.OracleDriver', \
+                           'jdbc:oracle:thin:@//localhost:41521/xe', \
+                           [ 'mydb', 'mydb']
+conn = jaydebeapi.connect(driver, url, driver_args)
 curs = conn.cursor()
 try:
     curs.execute("DROP TABLE Article")
@@ -12,8 +12,10 @@ curs.execute("CREATE TABLE Article (ID INTEGER PRIMARY KEY," \
              "Subject VARCHAR(256)," \
              "Body CLOB" \
 #             "Body LONG VARCHAR" \
+             ",Created TIMESTAMP" \
              ")")
-curs.execute("insert into Article values (1, 'my subject', 'A CLOB might be large')")
+curs.execute("insert into Article values (1, 'my subject', 'A CLOB might be large', TO_DATE('1890/05/03 21:02:44', 'yyyy/mm/dd hh24:mi:ss'))")
+#SYSDATE)")
 curs.execute("insert into Article (ID, Subject) values (2, 'my 2nd subject')")
 curs.execute("select * from Article")
 print curs.description[2]
