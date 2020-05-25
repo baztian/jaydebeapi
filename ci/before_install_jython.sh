9a0aadb0a1f9f3fe4346df513f89571115f52748
baztian <bastian.dev@gmail.com>
2020-05-25 20:02:04 +0200

    Fix tox for Jython

--- ci/before_install_jython.sh
+++ ci/before_install_jython.sh
@@ -5,7 +5,8 @@ INST_DIR=$HOME/jython/jython-${JYTHON##*:}
 if [ ! -d "$INST_DIR" ]; then
     JYTHON_JAR=$(${TRAVIS_BUILD_DIR}/ci/mvnget.sh "$JYTHON")
     java -jar ${JYTHON_JAR} -s -d "$INST_DIR"
-    $HOME/jython/bin/pip install --upgrade pip
+    "$INST_DIR"/bin/pip install --upgrade pip
 fi
+pip install --upgrade virtualenv==16.5.0
 mkdir -p $HOME/bin
 ln -s "$INST_DIR"/bin/jython $HOME/bin/
