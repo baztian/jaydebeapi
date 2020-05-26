06f7a438c586ba24f18bca0138e6a02b1557ac9c
baztian <bastian.dev@gmail.com>
2020-05-26 15:41:36 +0200

    Downgrade coverage dependency for Jython runs as Jython doesn't seem to have sqlite3 lib

--- ci/before_install_jython.sh
+++ ci/before_install_jython.sh
@@ -6,6 +6,6 @@ if [ ! -d "$INST_DIR" ]; then
     JYTHON_JAR=$(${TRAVIS_BUILD_DIR}/ci/mvnget.sh "$JYTHON")
     java -jar ${JYTHON_JAR} -s -d "$INST_DIR"
 fi
-pip install --upgrade virtualenv==15.0.1
+pip install --upgrade virtualenv==15.0.1 tox==3.9.0 coverage==4.5.4
 mkdir -p $HOME/bin
 ln -s "$INST_DIR"/bin/jython $HOME/bin/
