5b80ee6e1030c6b8a1df305f83011c24b62d4611
baztian <bastian.dev@gmail.com>
2020-05-27 20:52:10 +0200

    Revert "Another try with virtualenv 16.5.0"
    This reverts commit 94247cb2f89ee9d4976caffc557e816370fcc597.

--- ci/before_install_jython.sh
+++ ci/before_install_jython.sh
@@ -5,8 +5,7 @@ INST_DIR=$HOME/jython/jython-${JYTHON##*:}
 if [ ! -d "$INST_DIR" ]; then
     JYTHON_JAR=$(${TRAVIS_BUILD_DIR}/ci/mvnget.sh "$JYTHON")
     java -jar ${JYTHON_JAR} -s -d "$INST_DIR"
-    "$INST_DIR"/bin/pip install --upgrade pip
 fi
-pip install --upgrade virtualenv==16.5.0 tox==3.9.0 coverage==4.5.4
+pip install --upgrade virtualenv==15.1.0 tox==3.9.0 coverage==4.5.4
 mkdir -p $HOME/bin
 ln -s "$INST_DIR"/bin/jython $HOME/bin/
