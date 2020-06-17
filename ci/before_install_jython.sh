ab925619d6899a1f52a07bf9c3b099cafab17fe7
baztian <bastian.dev@gmail.com>
2020-06-17 22:47:47 +0200

    Add Jython support

--- ci/before_install_jython.sh
+++ ci/before_install_jython.sh
@@ -3,9 +3,8 @@ set -e
 
 INST_DIR=$HOME/jython/jython-${JYTHON##*:}
 if [ ! -d "$INST_DIR" ]; then
-    JYTHON_JAR=$(${TRAVIS_BUILD_DIR}/ci/mvnget.sh "$JYTHON")
+    JYTHON_JAR=$(${PWD}/ci/mvnget.sh "$JYTHON")
     java -jar ${JYTHON_JAR} -s -d "$INST_DIR"
 fi
 pip install --upgrade virtualenv==15.1.0 tox==3.9.0 coverage==4.5.4
-mkdir -p $HOME/bin
-ln -s "$INST_DIR"/bin/jython $HOME/bin/
+sudo ln -s "$INST_DIR"/bin/jython /usr/local/bin/
