d908f1f7139fa25ca364d265ad038e2d1a58b1da
baztian <bastian.dev@gmail.com>
2020-06-19 20:57:27 +0200

    Use Coveralls action and remove redundant coverage install from jython setup file

--- ci/before_install_jython.sh
+++ ci/before_install_jython.sh
@@ -6,5 +6,5 @@ if [ ! -d "$INST_DIR" ]; then
     JYTHON_JAR=$(${PWD}/ci/mvnget.sh "$JYTHON")
     java -jar ${JYTHON_JAR} -s -d "$INST_DIR"
 fi
-pip install --upgrade virtualenv==15.1.0 tox==3.9.0 coverage==4.5.4
+pip install --upgrade virtualenv==15.1.0 tox==3.9.0
 sudo ln -s "$INST_DIR"/bin/jython /usr/local/bin/
