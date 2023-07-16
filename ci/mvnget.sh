7f0f9c1d1fae9441bf787527d8954486687a8283
HenryNebula <c-huang2015011098@hotmail.com>
2023-07-15 21:24:31 -0400

    update old codebase

--- ci/mvnget.sh
+++ ci/mvnget.sh
@@ -8,7 +8,18 @@ ARTIFACT_NAME=`python -c "import re;print(re.search(r':(.*):', '$ARTIFACT_SPEC')
 _PATH=${GROUP_ID/./\/}/$ARTIFACT_NAME
 _ARTIFACT_SPEC_BASENAME=${NON_GROUP_ID/:/-}
 VERSION=${ARTIFACT_SPEC##*:}
-echo "Downloading ${ARTIFACT_NAME} version ${VERSION} group id ${GROUP_ID}..." >&2
-wget https://search.maven.org/remotecontent?filepath=${_PATH}/$VERSION/${_ARTIFACT_SPEC_BASENAME}.jar -O ${_ARTIFACT_SPEC_BASENAME}.jar
-echo "...download of ${_ARTIFACT_SPEC_BASENAME}.jar finished." >&2
-echo ${_ARTIFACT_SPEC_BASENAME}.jar
+JAR=${_ARTIFACT_SPEC_BASENAME}.jar
+if [ $# -ge 2 ]; then
+    OUTPUT_DIR="$2"
+else
+    OUTPUT_DIR="./"
+fi
+JAR_FULL_PATH=${OUTPUT_DIR}/${_ARTIFACT_SPEC_BASENAME}.jar
+if [ -f "$JAR_FULL_PATH" ]; then
+    echo "File $JAR_FULL_PATH exists."
+else
+    echo "File $JAR_FULL_PATH does not exist. Start downloading .. "
+    echo "Downloading ${ARTIFACT_NAME} version ${VERSION} group id ${GROUP_ID}..." >&2
+    wget https://search.maven.org/remotecontent?filepath=${_PATH}/$VERSION/${_ARTIFACT_SPEC_BASENAME}.jar -O $JAR_FULL_PATH
+    echo "...download of ${_ARTIFACT_SPEC_BASENAME}.jar finished." >&2
+fi
